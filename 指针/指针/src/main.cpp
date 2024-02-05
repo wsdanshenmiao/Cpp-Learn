331b13669766d41f49c7e8572543d2104004d12a
wsdanshenmiao <wsdanshenmiao@outlook.com>
2024-02-05 15:20:09 +0800

    指针

--- 指针/指针/src/main.cpp
+++ 指针/指针/src/main.cpp
@@ -1,19 +1,59 @@
 #include <iostream>
 #include <vector>
 
-void ForEach(std::vector<int>& values, void(*print)(int))
+//this指针
+class Person
 {
-	for (int value : values) {
-		print(value);
+public:
+
+	Person(int age)
+	{
+		//1、当形参和成员变量同名时，可用this指针来区分
+		this->age = age;
 	}
+
+	Person& PersonAddPerson(Person p)
+	{
+		this->age += p.age;
+		//返回对象本身
+		return *this;
+	}
+
+	int age;
+};
+
+void test01()
+{
+	Person p1(10);
+	std::cout << "p1.age = " << p1.age << std::endl;
+
+	Person p2(10);
+	p2.PersonAddPerson(10).PersonAddPerson(10).PersonAddPerson(10);
+	std::cout << "p2.age = " << p2.age << std::endl;
 }
 
-int main()
-{
-	std::vector<int> values = { 0,1,2,3,4,5 };
-	auto lambda = [](int value) {std::cout << value << std::endl; };
-	ForEach(values, lambda);
+int main() {
+
+	test01();
+
+	system("pause");
+
+	return 0;
 }
+
+//void ForEach(std::vector<int>& values, void(*print)(int))
+//{
+//	for (int value : values) {
+//		print(value);
+//	}
+//}
+//
+//int main()
+//{
+//	std::vector<int> values = { 0,1,2,3,4,5 };
+//	auto lambda = [](int value) {std::cout << value << std::endl; };
+//	ForEach(values, lambda);
+//}
 
 ////函数指针
 //void Print(int value)
