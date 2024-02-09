4546c0884af7b8d1448efdeb79392f7501251b29
wsdanshenmiao <wsdanshenmiao@outlook.com>
2024-02-09 11:28:28 +0800

    commit

--- C++类的继承/C++类的继承/src/main.cpp
+++ C++类的继承/C++类的继承/src/main.cpp
@@ -1,29 +1,58 @@
 #include <iostream>
 
-class Entity
+//菱形继承
+//两个派生类继承同一个基类
+//?又有某个类同时继承者两个派生类
+//?这种继承被称为菱形继承，或者钻石继承
+class Animal
 {
 public:
-	float X, Y;
-
-	void Move(float ax, float ay)
-	{
-		X += ax;
-		Y += ay;
-	}
+	int m_Age;
 };
 
-class Player :public Entity
-{
-public:
-	const char* name;
+class Tiger :virtual public Animal {};
 
-	void PrintName()
-	{
-		std::cout << name << std::endl;
-	}
-};
+class Lion :virtual public Animal{};
+
+class Liger :public Tiger, public Lion{};
 
 int main()
 {
+	Liger st;
+	st.Tiger::m_Age = 100;
+	st.Lion::m_Age = 200;
 
+	std::cout << st.Tiger::m_Age << std::endl;
+	std::cout << st.Lion::m_Age << std::endl;
+	std::cout << st.m_Age << std:: endl;
 }
+
+//#include <iostream>
+//
+//class Entity
+//{
+//public:
+//	float X, Y;
+//
+//	void Move(float ax, float ay)
+//	{
+//		X += ax;
+//		Y += ay;
+//	}
+//};
+//
+//class Player :public Entity
+//{
+//public:
+//	const char* name;
+//
+//	void PrintName()
+//	{
+//		std::cout << name << std::endl;
+//	}
+//};
+//
+//int main()
+//{
+//
+//}
--- C++虚函数/C++虚函数/src/main.cpp
+++ C++虚函数/C++虚函数/src/main.cpp
@@ -1,40 +1,76 @@
 #include <iostream>
 
-class Printable	//接口
+class Entity
 {
 public:
-	virtual std::string GetClassName() = 0;	//构造纯虚函数
+	virtual std::string GetName() { return "Entity"; }	//虚函数
+	virtual ~Entity() = 0;	//声明一个纯虚函数
 };
 
-class Entity : public Printable
-{
-public:
-	virtual std::string GetName() { return "Entity"; }
-	std::string GetClassName() override { return "Entity"; }
-};
+Entity::~Entity(){}	//纯虚函数必须具体实现函数
 
 class Player :public Entity
 {
 private:
-	std::string m_name;
+	std::string* m_name;
 public:
 	Player(const std::string& name)
-		:m_name(name) {}
-	std::string GetName() override { return m_name; }
-	std::string GetClassName() override { return "Player"; }
+	{
+		m_name = new std::string(name);	//在堆区创建内存
+	}
+	~Player()
+	{
+		if (m_name) {	//若m_name不为空，释放内存
+			delete m_name;
+			m_name = NULL;
+		}
+	}
+	std::string GetName() override { return *m_name; }	//覆写函数
 };
-
-void PrintName(Entity* entity)
-{
-	std::cout << entity->GetClassName() << std::endl;
-}
 
 int main()
 {
-	Entity* e = new Entity();
-	Player* p = new Player("wsdsm");
-	PrintName(e);
-	PrintName(p);
-	delete e;
-	delete p;
+	Entity* p = new Player("wsdsm");
+	std::cout << p->GetName() << std::endl;
 }
+
+//#include <iostream>
+//
+//class Printable	//接口
+//{
+//public:
+//	virtual std::string GetClassName() = 0;	//构造纯虚函数
+//};
+//
+//class Entity : public Printable
+//{
+//public:
+//	virtual std::string GetName() { return "Entity"; }
+//	std::string GetClassName() override { return "Entity"; }
+//};
+//
+//class Player :public Entity
+//{
+//private:
+//	std::string m_name;
+//public:
+//	Player(const std::string& name)
+//		:m_name(name) {}
+//	std::string GetName() override { return m_name; }
+//	std::string GetClassName() override { return "Player"; }
+//};
+//
+//void PrintName(Entity* entity)
+//{
+//	std::cout << entity->GetClassName() << std::endl;
+//}
+//
+//int main()
+//{
+//	Entity* e = new Entity();
+//	Player* p = new Player("wsdsm");
+//	PrintName(e);
+//	PrintName(p);
+//	delete e;
+//	delete p;
+//}
