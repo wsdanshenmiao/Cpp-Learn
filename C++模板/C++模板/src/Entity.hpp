17ac8e46548744caed9a5d17505bb445b1287fe8
wsdanshenmiao <wsdanshenmiao@outlook.com>
2024-02-14 00:05:46 +0800

    C++模板

---
+++ C++模板/C++模板/src/Entity.hpp
@@ -0,0 +1,24 @@
+#ifndef __ENTITY__HPP__
+#define __ENTITY__HPP__
+
+#include <string>
+
+template<typename T>
+class Entity {
+public:
+	T m_X;
+	T m_Y;
+	Entity(T x, T y);
+	void Function();
+};
+
+template<typename T>
+Entity<T>::Entity(T x, T y)
+	:m_X(x),m_Y(y){}
+
+template<typename T>
+void Entity<T>::Function()
+{
+}
+
+#endif // !__ENTITY__HPP__
